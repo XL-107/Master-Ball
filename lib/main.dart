fcd2065fa7acd07e2b9761154b31617a6bf7de4b
XL-107 <xavier@LAPTOP-R0RJ06RL.localdomain>
2026-02-20 18:45:53 -0500

    Now properly accesses database information; also edited database format slightly

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,55 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
-import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  
-
-  
-  runApp(const MainApp());
+void main(){
+  runApp(const MyApp());
 }
 
-class PokemonBasic{ //just name and types, no other data (for now)
-  final int ID;
-  final String Name;
-  final String Type1;
-  final String? Type2;
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  const PokemonBasic({required this.ID, required this.Name, required this.Type1, required this.Type2});
+  Future<Database> initDB() async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, 'newtable.db');
 
-  String getName(){
-    return Name;
+    return openDatabase(
+      path,
+      version: 1,
+      
+      onConfigure:(db) async {
+        db.execute('DROP TABLE IF EXISTS pokemon_test');
+      },
+      onOpen: (db) async {
+        await db.execute('''
+          CREATE TABLE pokemon_test (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            type1 TEXT NOT NULL,
+            type2 TEXT
+          )
+        ''');
+        final data = await rootBundle.loadString('assets/pokemon_test.sql');
+        final lines = data.split('\n');
+        Batch batch = db.batch();
+        for (var i=8; i<1033; i++){
+          batch.rawInsert(lines[i]);
+        }
+        await batch.commit(noResult: true);
+      },
+    );
   }
-}
 
+  Future<List<Map<String, dynamic>>> getPokemon() async {
+    final db = await initDB();
+    return db.query('pokemon_test');
+  }
 
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  Future<PokemonBasic?> queryIndex(int index) async { //returns only a single pokemon at a time
-    final database = openDatabase(join(await getDatabasesPath(), 'pokemon_text.sql')); //set path to database and store reference
-    final db = await database;
-    final List<Map<String, Object?>> maps = await db.query("pokemon_test", where: 'ID = ?', whereArgs: [index]);
-
-    if (maps.isNotEmpty){ //return pokemon info, but only id, name, type1, and type2
-      return PokemonBasic(ID: maps.first["ID"] as int, Name: maps.first["Name"] as String, Type1: maps.first["Type1"] as String, Type2: maps.first["Type2"] as String);
-    }
-    return null; //in case index is invalid
+  Future<String> getNameAtIndex(int index) async {
+    final db = await initDB();
+    final result = await db.query('pokemon_test', where: 'id = ?', whereArgs: [index+1]);
+    return result[0]['name'] as String;
   }
 
   @override
@@ -48,32 +60,27 @@ class MainApp extends StatelessWidget {
         body: ListView.builder(
           itemCount: 1025,
           itemBuilder:(BuildContext context, int index) {
-            return FutureBuilder<PokemonBasic?>(
-              future: queryIndex(index),
+            return FutureBuilder<String>(
+              future: getNameAtIndex(index),
               builder: (context, snapshot) {
-              
                 if (snapshot.connectionState == ConnectionState.waiting) {
                   return Container(
                     height: 50,
-                    child: Text("..."), //data has yet to load
+                    child: Text("Loading..."), //data has yet to load
                   );
                 }
 
-                //Error checking
                 if (!snapshot.hasData || snapshot.data == null) {
                   return Container(
                     height: 50,
-                    child: Text("ERROR"),
+                    child: Text("no snapshot data"),
                   );
                 }
 
-                final pokemon = snapshot.data!; //idk what this actually does, i got it from chatgpt (TODO: figure out what snapshots are)
-              
                 return Container(
                   height: 50,
-                  child: Text(pokemon.Name)
+                  child: Text(snapshot.data!),
                 );
-
               }
             );
             
