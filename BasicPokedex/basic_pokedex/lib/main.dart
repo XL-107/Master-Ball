233f49cb1aaa7538a400afb4f507350f5b5433e5
nolasilman <alonl828@gmail.com>
2026-03-14 20:06:05 -0400

    Pokedex using PokeAPI

--- BasicPokedex/basic_pokedex/lib/main.dart
+++ BasicPokedex/basic_pokedex/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'pokemon.dart';
+import 'pokeApiService.dart';
 
 void main() {
   runApp(MainApp());
@@ -12,62 +14,62 @@ class MainApp extends StatefulWidget {
 }
 
 class _MainAppState extends State<MainApp> {
-  final List<Pokemon> pokemonList = [ //list with first 3 pokemon. Needs to be changed to be implemented with database
-    Pokemon('Bulbasaur', 'Stage 1', 'assets/001.png'),
-    Pokemon('Ivysaur', 'Stage 2', 'assets/002.png'),
-    Pokemon('Venasaur', 'Stage 3', 'assets/003.png'),
-  ];
-
+  final _service = PokeApiService();
+  late Future<List<Pokemon>> _pokemonFuture;
   int currentIndex = 0;
 
-  void next() { //increments index to show next pokemon in dex array
-    setState(() {
-      currentIndex = (currentIndex + 1) % pokemonList.length;
-    });
-  }
-
-  void previous() { //decrements index to show next pokemon in dex array
-    setState(() {
-      currentIndex = (currentIndex - 1 + pokemonList.length) % pokemonList.length;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _pokemonFuture = _service.fetchOriginal151();
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Image.asset(pokemonList[currentIndex].imagePath), //displays image
-              Text('${pokemonList[currentIndex]}'), //prints text from pokemon toString function
-              Row(
+        body: FutureBuilder<List<Pokemon>>(
+          future: _pokemonFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            }
+
+            final pokemonList = snapshot.data!;
+
+            return Center(
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: [ //previous and next buttons. Should be replaced/shared with swipes to prioritize for mobile
-                  ElevatedButton(onPressed: previous, child: Text('Previous')),
-                  SizedBox(width: 16),
-                  ElevatedButton(onPressed: next, child: Text('Next')),
+                children: [
+                  Image.network(pokemonList[currentIndex].imageUrl),
+                  Text('${pokemonList[currentIndex]}'),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(
+                        onPressed: () => setState(() { //previous button
+                          currentIndex = (currentIndex - 1 + pokemonList.length) % pokemonList.length; //decrements index. mod so it can loop
+                        }),
+                        child: Text('Previous'),
+                      ),
+                      SizedBox(width: 16),
+                      ElevatedButton(
+                        onPressed: () => setState(() { //next button
+                          currentIndex = (currentIndex + 1) % pokemonList.length; //increments index. mod so it can loop
+                        }),
+                        child: Text('Next'),
+                      ),
+                    ],
+                  )
                 ],
-              )
-            ],
-          ),
+              ),
+            );
+          },
         ),
       ),
     );
-  }
-}
-
-class Pokemon{
-  String name;
-  String stage;
-  String imagePath;
-  //need to add whatever other info we want to display
-
-  Pokemon(this.name, this.stage, this.imagePath);
-
-  @override
-  String toString() {
-    return 'Pokemon(name: $name, stage: $stage)';
   }
 }
---
+++ BasicPokedex/basic_pokedex/lib/pokemon.dart
@@ -0,0 +1,34 @@
+class Pokemon{
+  final int id;
+  final String name;
+  final List<String> types;
+  final String imageUrl;
+  
+  Pokemon({ //pokemon class constructor
+    required this.id,
+    required this.name,
+    required this.types,
+    required this.imageUrl,
+  });
+
+   factory Pokemon.fromJson(Map<String, dynamic> json) { //factory contructor parses json and uses regular constructor
+
+    List<String> typeList = []; //loops through types in json to add to typeList array
+    for(var t in json['types'] as List){
+      typeList.add(t['type']['name'] as String);
+    }
+
+    return Pokemon( //passes json info to constructor
+      id: json['id'],
+      name: json['name'],
+      types: typeList,
+      imageUrl: json['sprites']['other']['official-artwork']['front_default'] ?? 
+                json['sprites']['front_default'],
+    );
+  }
+
+  @override
+  String toString() {
+    return 'ID: $id, Name: $name, Type(s): $types';
+  }
+}
