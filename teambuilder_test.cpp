90ae265def040f8182c423fe40086ce102ba9d80
Marco Quintero <maq1810787@gmail.com>
2026-03-17 11:31:50 -0400

    Added connections from the teambuilder algorithm to the pokemon_test database

--- teambuilder_test.cpp
+++ teambuilder_test.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 #include <map>
 #include <sqlite3.h>
 typedef std::map<std::string, double> TYPE_MATCHUPS;
+//Gets the type of the Pokemon from the pokemon database
+std::vector<std::string> get_pokemon(std::string pokemon){
+    std::vector<std::string> type;
+    sqlite3* db;
+    sqlite3_stmt* stmt;
+    sqlite3_open("pokemon_test.db", &db);
+    std::string query = "SELECT Type1, Type2 FROM pokemon_test WHERE Name = '" + pokemon + "'";
+    sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, NULL);
+    if(sqlite3_step(stmt) == SQLITE_ROW){
+        type.push_back((const char*)sqlite3_column_text(stmt, 0));
+        if(sqlite3_column_type(stmt,1) != SQLITE_NULL){
+            type.push_back((const char*)sqlite3_column_text(stmt, 1));
+        }else {
+            type.push_back("None");
+        }
+    }else {
+        std::cout << "Pokemon not found \n";
+    }
+    return type;
+}
 //Converts the column with the specified header into a map that contains all of its weaknesses, immunities, and resistances
 TYPE_MATCHUPS get_type(std::string type){
     TYPE_MATCHUPS matchups;
@@ -16,6 +37,7 @@ TYPE_MATCHUPS get_type(std::string type){
     }
     std::string query = "SELECT Type, " + type + " FROM type_chart";
     //built-in function that compiles the SQLite query that will then be executed
+    //std::cout << "Running query: " << query << std::endl;
     sqlite3_prepare_v2(db, query.c_str(), -1, &stmt, NULL);
     while(sqlite3_step(stmt) == SQLITE_ROW){
         std::string defending_type = (const char*)sqlite3_column_text(stmt,0);
@@ -43,11 +65,23 @@ TYPE_MATCHUPS defense_calc(TYPE_MATCHUPS type1, TYPE_MATCHUPS type2) {
 }
 
 int main(int argc, char const *argv[]){
+    /*
     TYPE_MATCHUPS swampert_matchup = defense_calc(get_type("Water"), get_type("Ground"));
     TYPE_MATCHUPS::iterator swampert_iter = swampert_matchup.begin();
     while (swampert_iter != swampert_matchup.end()){
-        std::cout << swampert_iter->first << " " << swampert_iter->second;
+        std::cout << swampert_iter->first << " " << swampert_iter->second << std::endl;
         ++swampert_iter;
+    }*/
+   
+    std::string type1 = get_pokemon(argv[1])[0];
+    //std::cout << type1 << std::endl;
+    std::string type2 = get_pokemon(argv[1])[1];
+    //std::cout << type2 << std::endl;
+    TYPE_MATCHUPS matchup = defense_calc(get_type(type1), get_type(type2));
+    TYPE_MATCHUPS::iterator iter = matchup.begin();
+    while(iter != matchup.end()){
+        std::cout << iter->first << " " << iter->second << std::endl;
+        ++iter;
     }
     return 0;
 }
