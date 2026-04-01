d966d6fc55a6af9d448eb39883762575adf92a74
sophie343liu <sophie343liu@gmail.com>
2026-03-31 21:58:01 -0400

    Took note of pretty much every special form case
    Also noticed we're missing every gmax. gonna have to add those later.

--- obtain_abilities.py
+++ obtain_abilities.py
@@ -3,23 +3,83 @@ import sqlite3
 import pandas as pd
 
 def get_all_pokemon():
-    query = "SELECT DISTINCT Name FROM expanded_pokemon_test"
+    query = "SELECT DISTINCT Name, Form FROM expanded_pokemon_test WHERE Form IS NOT NULL"
 
     conn = sqlite3.connect('MasterBall.db')
     db = pd.read_sql(query, conn)
     
-    for x in db.head(5)["Name"].items():
-        print(x[1])
+    for x in db.itertuples():
+        print(x)
     
     
     return
 
 def abilityAPI():
     # FORM HANDLING: 
-    # EX: gengar-mega, gengar-gmax, ninetails-alola, 
-    # Partner Pikachu -> pikachu-starter
-    # Tauros breeds -> tauros-paldea-combat-breed
-    # ??? 
+    # Megas: <name>-mega
+    #      X/Y: <name>-mega-x  
+    # Gmax: <name>-gmax
+    # Alolan: <name>-alola
+    # Galarian: <name>-galar
+    # Hisuian: <name>-hisui
+    # Paldean: wooper-paldea
+    # Primal: kyogre-primal groudon-primal
+    # 
+    # OTHER CASES: 
+    # Deoxys: deoxys-<normal/attack/defense/speed> 
+    # Partner Pikachu/Eevee: pikachu-starter eevee-starter
+    # Castform: castform, castform-<rainy/sunny/snowy> 
+    # Tauros breeds: tauros-paldea-<aqua/blaze/combat>-breed
+    # Burmy/Wormadam: wormadam-<plant/sandy/trash>
+    #       All burmy forms are the same and do not have a unique URL.
+    # Rotom: rotom, rotom-<fan/frost/heat/mow/wash>
+    # dialga-origin, palkia-origin, giratina-origin, giratina-altered
+    # Shaymin: <>-land, <>-sky
+    # Basculin: <>-red-striped, <>-blue-striped, <>-white-striped
+    # Darmanitan: <>-standard, <>-zen, <>-galar-standard, <>-galar-zen
+    # Tornadus/Thundurus/Landorus/Enamorus: <>-incarnate, <>-therian
+    # Kyurem: <>, <>-black, <>-white
+    # Keldeo: <>-ordinary, <>-resolute
+    # Meloetta: <>-aria, <>-pirouette
+    # Greninja: <>-ash
+    # Meowstic: <>-female, <>-male
+    # Aegislash: <>-blade, <>-shield
+    # Pumpkaboo/Gourgeist: <>-<average/large/small/super> 
+    # Zygarde: <>-10, <>-50, <>-10-power-construct, <>-50-power-construct, <>-complete
+    #       "power-construct" is a special ability. Probably best to put it into ability2 or something...
+    # Hoopa: <>, <>-unbound (confined needs to use default instead)
+    # Oricorio: <>-baile, <>-pau, <>-pom-pom, <>-sensu
+    # Rockruff: <>, <>-own-tempo
+    # Lycanroc: <>-dusk, <>-midday, <>-midnight
+    # Wishiwashi: <>-school, <>-solo
+    # Minior: <>-<red/orange/yellow/green/blue/indigo/violet> (core)
+    #         <>-<red/orange/yellow/green/blue/indigo/violet>-meteor
+    #         note: all of these have the same ability...
+    # Necrozma: <>-dusk, <>-dawn, <>-ultra
+    # Toxtricity: <>-low-key, <>-amped     (<>-gmax)
+    # Eiscue: <>-ice, <>-noice
+    # Indeedee: <>-female, <>-male
+    # Morpeko: <>-full-belly, <>-hangry
+    # Zacian/Zamazenta: <>, <>-crowned
+    # Eternatus: <>, <>-eternamax
+    # Urshifu: <>-single-strike, <>-rapid-strike
+    # Calyrex: <>, <>-ice, <>-shadow
+    # Ursaluna: <>, <>-bloodmoon
+    # Basculegion: <>-male, <>-female
+    # Oinkologne: <>-male, <>-female
+    # Maushold: <>-family-of-three, <>-family-of-four
+    # Squawkabilly: <>-<blue/green/white/yellow>-plumage
+    #           These DO have different abilities depending on plumage. 
+    # Palafin: <>-hero, <>-zero
+    # Tatsugiri: <>-<curly/droopy/stretchy>
+    # Dudunsparce: <>-<two/three>-segment
+    # Gimmighoul: <>, <>-roaming (chest is default)
+    # Ogerpon: <>, <>-<cornerstone/hearthflame/wellspring>-mask (teal is default)
+    # Terapagos: <>, <>-stellar, <>-terastal
+
+
+
+
 
     url = "https://pokeapi.co/api/v2/pokemon/Vanillite"
     response = requests.get(url)
@@ -31,6 +91,6 @@ def abilityAPI():
 
 
 def main():
-    abilityAPI()
+    get_all_pokemon()
 
 main()
