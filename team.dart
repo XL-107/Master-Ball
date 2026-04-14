9a84ff5268049a3d5e30f2fd728d4f5393a599f4
Henry Xue <honglin.xue1@gmail.com>
2026-04-14 16:47:07 -0400

    Tried making it so that teams can conver to json

--- team.dart
+++ team.dart
@@ -1,13 +1,29 @@
 import 'pokemon.dart';
+import 'dart:convert';
 
 class Team {
   String name;
   List<Pokemon> members;
 
-  Team(this.name, this.members);
+  Team({required this.name, required this.members});
+
+  factory Team.fromJson(Map<String, dynamic> json) => Team(
+    name : json['name'],
+    members: json['members'],
+    );
+
+  Map<String, dynamic> toJson() => {
+    'name': name,
+    'members': members,
+  };
+  
 
   void addPokemon(Pokemon p){
     members.add(p);
+  }
+
+  void removePokemon(int index){
+    members.removeAt(index);
   }
 
 }
